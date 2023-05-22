c4c07f040ee694dbf0e9cd79da4a02961a9dd6a3
Farouq Siyar <siyar9989@gmail.com>
2023-05-22 12:19:45 +0200

    refine NavBar, add logo

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import "./NavBar.css"
+import Logo from '../../assets/luminary_nobg.png'
 
 export default function NavBar() {
   return (
     <nav className='navbar'>
-      <div>Logo</div>
+      <img className='logo' src={Logo}/>
       <ul className="nav-elements">
-        <li className="nav-element">Home</li>
-        <li className="nav-element">Schedule</li>
-        <li className="nav-element">Movies</li>
-        <li className="nav-element">News</li>
+        <li className="nav-element nav-point">Home</li>
+        <li className="nav-element nav-point">Schedule</li>
+        <li className="nav-element nav-point">Movies</li>
+        <li className="nav-element nav-point">News</li>
         <li className="nav-element" style={{fontSize:"1.5rem"}}>🔍</li>
         <li className="nav-element" id='sign-in-btn'>Sign In</li>
       </ul>
