7643f27b6442f28d7b39c61d6e78968dadb1ae97
Farouq Siyar <siyar9989@gmail.com>
2023-05-24 16:32:33 +0200

    add movie types on ComingMovies component, refine design

--- frontend/src/Pages/HomePage/ComingMovies/ComingMovies.jsx
+++ frontend/src/Pages/HomePage/ComingMovies/ComingMovies.jsx
@@ -7,9 +7,16 @@ export default function ComingMovies() {
       <section className="coming-movies-section">
         <div className="subtitle">
           <div className="subtitle-text">Opening this week</div>
-          <div className="opening-week-search">search</div>
+          <input className="opening-week-search" type="search" placeholder="Search..." />
         </div>
         <div className="section-line"></div>
+        <ul className="coming-movies-types">
+          <li className="movie-type">All films</li>
+          <li className="movie-type">2D</li>
+          <li className="movie-type">3D</li>
+          <li className="movie-type">IMAX</li>
+          <li className="movie-type">IMAX 3D</li>
+        </ul>
         <div className="coming-movies-grid"></div>
       </section>
     </div>
