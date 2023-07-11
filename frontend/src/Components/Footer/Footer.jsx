a3c06fe63b90d928e52e322ea1234bfc170a9c4a
Farouq Siyar <siyar9989@gmail.com>
2023-07-11 13:44:57 +0200

    put basic text/info on Footer

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,9 +1,15 @@
 import "./Footer.css"
 
 export default function Footer() {
+
+  const year = new Date().getFullYear();
+  const address = "1065 Budapest, Nagymező u. 44."
+  const phoneNum = "06 1 234 5678"
+  const email = "luminary@cinema.com"
+
   return (
     <div className="footer">
-      This is the footer.
+      © {year} Luminary Cinema. All rights reserved. | {address} | Phone: {phoneNum} | Email: {email}
     </div>
   )
 }
