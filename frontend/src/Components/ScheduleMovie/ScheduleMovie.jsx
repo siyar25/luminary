cbcb240bc39a1d14912f16ed6bb49ccdbc2bdcef
Farouq Siyar <siyar9989@gmail.com>
2023-08-02 11:54:48 +0200

    fix days on schedule page

--- frontend/src/Components/ScheduleMovie/ScheduleMovie.jsx
+++ frontend/src/Components/ScheduleMovie/ScheduleMovie.jsx
@@ -4,15 +4,28 @@ import CalendarDay from "../CalendarDay/CalendarDay";
 import { useEffect, useRef, useState } from "react";
 
 export default function ScheduleMovie() {
-  const [activeDay, setActiveDay] = useState(null);
+  const [activeDay, setActiveDay] = useState(
+    new Date().toLocaleString("en-us", { weekday: "long" })
+  );
 
-  useEffect(() => {
-    const today = new Date().toLocaleString('en-us', {weekday: 'long'})
-    setActiveDay(today);
-  }, [])
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  const weekArray = Array.from(Array(7).keys()).map((idx) => {
+    const d = new Date();
+    d.setDate(d.getDate() - d.getDay() + 1 + idx);
+    return d;
+  });
 
   function isActive(day) {
-    setActiveDay(day)
+    setActiveDay(day);
   }
 
   return (
@@ -24,7 +37,7 @@ export default function ScheduleMovie() {
             <div className="movie-top-info-title">
               Venom <span>13+</span>
             </div>
-            <div className="movie-top-info-imdb">IMDb 6.6</div>
+            <a className="movie-top-info-imdb" href="https://www.imdb.com" target="blank">IMDb 6.6</a>
           </div>
           <div className="movie-info">
             <div className="movie-duration">120 min</div>
@@ -32,14 +45,23 @@ export default function ScheduleMovie() {
             <div className="movie-genres">ACTION</div>
           </div>
           <div className="movie-top-schedule">
-            <CalendarDay day={"Monday"} number={17} month={"Feb"} isActive={isActive} activeDay={activeDay}/>
-            <CalendarDay day={"Tuesday"} number={18} month={"Feb"} isActive={isActive} activeDay={activeDay}/>
-            <CalendarDay day={"Wednesday"} number={19} month={"Feb"} isActive={isActive} activeDay={activeDay}/>
-            <CalendarDay day={"Thursday"} number={20} month={"Feb"} isActive={isActive} activeDay={activeDay}/>
-            <CalendarDay day={"Friday"} number={21} month={"Feb"} isActive={isActive} activeDay={activeDay}/>
-            <CalendarDay day={"Saturday"} number={22} month={"Feb"} isActive={isActive} activeDay={activeDay}/>
-            <CalendarDay day={"Sunday"} number={23} month={"Feb"} isActive={isActive} activeDay={activeDay}/>
-
+            {days.map((day) => {
+              const dayNum = weekArray[days.indexOf(day)].getDate();
+              const month = weekArray[days.indexOf(day)].toLocaleString(
+                "en-us",
+                { month: "long" }
+              );
+              return (
+                <CalendarDay
+                  day={day}
+                  number={dayNum}
+                  month={month}
+                  isActive={isActive}
+                  activeDay={activeDay}
+                  key={day}
+                />
+              );
+            })}
           </div>
           <div className="movie-top-arrows">
             <div className="movie-top-arrow">⬅</div>
@@ -49,15 +71,21 @@ export default function ScheduleMovie() {
       </div>
       <div className="movie-bottom">
         <div className="movie-bottom-row">
-          <div className="movie-bottom-type" style={{marginRight:`17%`}}>2D</div>
+          <div className="movie-bottom-type" style={{ marginRight: `17%` }}>
+            2D
+          </div>
           <div className="movie-bottom-screening">12:00</div>
         </div>
         <div className="movie-bottom-row">
-          <div className="movie-bottom-type" style={{marginRight:`35%`}}>3D</div>
+          <div className="movie-bottom-type" style={{ marginRight: `35%` }}>
+            3D
+          </div>
           <div className="movie-bottom-screening">15:00</div>
         </div>
         <div className="movie-bottom-row">
-          <div className="movie-bottom-type" style={{marginRight:`75%`}}>IMAX 3D</div>
+          <div className="movie-bottom-type" style={{ marginRight: `75%` }}>
+            IMAX 3D
+          </div>
           <div className="movie-bottom-screening">21:00</div>
         </div>
       </div>
